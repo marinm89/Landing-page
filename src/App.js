290b006368cf81e86cd4b5f40ec2575954acfb30
Marin Mrvoš <marin.mrvos@hotmail.com>
2020-09-05 16:46:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import hero from './images/hero-desktop.jpg';
 import './App.css';
+import arrow from './images/icon-arrow.svg'
+
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='description'>
+        <div className='text-part'>
+            <img src={logo}/>
+            <h1>WE'RE</h1>
+            <h2>COMING SOON</h2>
+            <p>
+              Hello fellow shoppers! We're currently building our new fashion store.
+              Add your email below to stay up-to-date with announcments and our launch deals.
+            </p>
+        </div>
+        <div className='form'>
+            <input type='email' placeholder='Email Address'/>
+            <button type='submit'><img src={arrow}/></button>
+      </div>  
+      </div>  
+      <div className='avatar'>
+        <img src={hero} />
+      </div>
     </div>
   );
 }
